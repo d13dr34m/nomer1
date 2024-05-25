645167a18704450cf8efc8b90b14508a8f9872e3
d13dr34m <goldobin-04@list.ru>
2024-05-25 14:52:53 +0300

    zadachanomer1.py

--- zadachanomer1.py
+++ zadachanomer1.py
@@ -5,24 +5,22 @@ import matplotlib.pyplot as plt
 
 
 def f(x):
-     return 100*np.sqrt(np.abs(1-0.001*x**2))+0.01*np.abs(x+10)
+ return 100*np.sqrt(np.abs(1-0.001*x**2))+0.01*np.abs(x+10)
 
 
 x = np.linspace(-15, 5, 400)
 y = f(x)
-z = list(x)
-v = list(y)
-Data = {
-    "x": z,
-    "y": v
-}
+data = [{"x": x, "y": y} for x, y in zip(x, y)]
+
+
 if not os.path.exists('results'):
-    os.makedirs('results')
+ os.makedirs('results')
+
 
 with open('results/values.json', 'w') as file:
     json.dump(data, file)
 plt.figure(figsize=(16, 9))
-plt.plot(x, y, label='f(x)', color='r')
+plt.plot(x, y, label='f(x)', color='k')
 plt.title('График')
 plt.xlabel('x')
 plt.ylabel('f(x)')
